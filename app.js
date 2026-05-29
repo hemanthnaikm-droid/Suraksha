38de8485d90593704088d9eba4288642dab123b6
hemanthnaikm-droid <mucchalhemanthnaik@gmail.com>
2026-05-29 11:23:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,3 +76,41 @@ function fakeCall() {
   alert("📞 Incoming Call From MOM");
   
 }
+let mediaRecorder;
+let audioChunks = [];
+
+async function startRecording() {
+
+  const stream =
+  await navigator.mediaDevices.getUserMedia({
+    audio: true
+  });
+
+  mediaRecorder =
+  new MediaRecorder(stream);
+
+  mediaRecorder.start();
+
+  mediaRecorder.ondataavailable = e => {
+    audioChunks.push(e.data);
+  };
+
+  mediaRecorder.onstop = () => {
+
+    const audioBlob =
+    new Blob(audioChunks, {
+      type: "audio/mp3"
+    });
+
+    const audioURL =
+    URL.createObjectURL(audioBlob);
+
+    const a =
+    document.createElement("a");
+
+    a.href = audioURL;
+    a.download = "emergency_recording.mp3";
+    a.click();
+
+  };
+}
